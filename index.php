c3f0642955984b5ec1194082f73f6938148207c7
asimchoudhury <asim.choudhury@gmail.com>
2016-11-09 01:55:01 +0530

    debug finished

--- index.php
+++ index.php
@@ -15,14 +15,14 @@ if (isset($_POST['submit'])) {
 	$username = $_POST['username'];
 	//$url = "http://myphpserver/RestExample/Src/Database.php?username=$username";//For Local Machine
 	$url = "https://enigmatic-waters-53240.herokuapp.com/Database.php?username=$username";
-	echo "url : $url";
+	//echo "url : $url";
 	$client = curl_init($url);
-	echo "</br>client : $client";
+	//echo "</br>client : $client";
 	curl_setopt($client, CURLOPT_RETURNTRANSFER, 1);
 	$response = curl_exec($client);
-	echo "<br>response : $response";
+	//echo "<br>response : $response";
 	$result = json_decode($response);
-	echo "<br>result : $response";
+	//echo "<br>result : $response";
 	if( empty($result) ) {
 		//var_dump($response);
 		echo '<b>User does not exist</b></br>';
