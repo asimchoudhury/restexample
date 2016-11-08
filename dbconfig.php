0d96e8659930c605621410de5e86409b37542ed6
asimchoudhury <asim.choudhury@gmail.com>
2016-11-09 00:46:40 +0530

    First Commit

---
+++ dbconfig.php
@@ -0,0 +1,12 @@
+<?php
+// Database Configuration
+
+// Hostname
+define ( 'DB_HOST', 'localhost' );
+// Username
+define ( 'DB_USER', 'root' );
+// Password
+define ( 'DB_PASSWORD', '' );
+// Databse Name
+define ( 'DB_DB', 'dblogin2' );
+?>
---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Acessing RestFUL APP via PHP</title>
+</head>
+<body>
+<form method="post" action="">
+<input type="text" name="username">
+<input type="submit" name="submit" value="Submit">
+</form>
+
+<?php
+
+if (isset($_POST['submit'])) {
+	$username = $_POST['username'];
+	$url = "http://myphpserver/RestExample/Src/Database.php?username=$username";
+	//echo "url : $url";
+	$client = curl_init($url);
+	//echo "</br>client : $client";
+	curl_setopt($client, CURLOPT_RETURNTRANSFER, 1);
+	$response = curl_exec($client);
+	//echo "<br>response : $response";
+	$result = json_decode($response);
+	//echo "<br>result : $response";
+	if( empty($result) ) {
+		//var_dump($response);
+		echo '<b>User does not exist</b></br>';
+	} else {
+		var_dump($response);
+	}
+}
+
+?>
+ 
+</body>
+</html>
