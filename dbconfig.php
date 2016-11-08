1d58ddfaacd9907a78ffb9a691c6e0d1a1b3b6cb
asimchoudhury <asim.choudhury@gmail.com>
2016-11-09 01:34:35 +0530

    making changes for dbconfig

--- dbconfig.php
+++ dbconfig.php
@@ -1,6 +1,29 @@
 <?php
 // Database Configuration
 
+/*
+ * Connection for Heroku Clear DB
+ */
+ $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+ $DB_host = $url["host"];
+ $DB_user = $url["user"];
+ $DB_pass = $url["pass"];
+ $DB_name = substr($url["path"], 1);
+
+// Hostname
+define ( 'DB_HOST', $DB_host);
+// Username
+define ( 'DB_USER', $DB_user );
+// Password
+define ( 'DB_PASSWORD', $DB_pass );
+// Databse Name
+define ( 'DB_DB', $DB_name );
+
+
+/*
+ * Connection for local Machine 
+ *
+
 // Hostname
 define ( 'DB_HOST', 'localhost' );
 // Username
@@ -8,5 +31,5 @@ define ( 'DB_USER', 'root' );
 // Password
 define ( 'DB_PASSWORD', '' );
 // Databse Name
-define ( 'DB_DB', 'dblogin2' );
+define ( 'DB_DB', 'dblogin2' );*/
 ?>
